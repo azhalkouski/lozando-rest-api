d2191706e0a1521ea53baf18536fae797c442098
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-11-01 19:54:23 +0300

    define endpoint GET /api/product/all

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './routes'
 
 const PORT = process.env.port || 3000;
 
 const app = express();
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
