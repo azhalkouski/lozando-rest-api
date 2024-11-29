6977c3dbda34c4f3f02fe4e96dfff0bfaf789d6c
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-11-29 23:53:40 +0300

    add MongoDB winston transport for level: 'warn'

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,7 +4,8 @@ import "winston-mongodb";
 const MONGODB_HOST = process.env.MONGODB_DATABASE_HOST;
 const MONGODB_NAME = process.env.MONGODB_DATABASE_NAME;
 const MONGODB_PORT = process.env.MONGODB_DATABASE_PORT;
-const ERROR_LOGS_COLLECTION = `${process.env.ERROR_LOGS_COLLECTION_NAME}`;
+const ERROR_LOGS_COLLECTION = `${process.env.ERROR_LOGS_COLLECTION}`;
+const WARNING_LOGS_COLLECTION = process.env.WARNING_LOGS_COLLECTION;
 
 if (!MONGODB_HOST || !MONGODB_PORT || !MONGODB_NAME) {
   throw new Error('Missing config data for MongoDB');
@@ -18,12 +19,6 @@ const colors = {
   debug: 'blue',
 };
 
-const mongoDbOptions = {
-  level: 'error',
-  db: `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`,
-  collection: ERROR_LOGS_COLLECTION,
-};
-
 winston.addColors(colors);
 
 const logger = winston.createLogger({
@@ -32,11 +27,20 @@ const logger = winston.createLogger({
   defaultMeta: { service: 'lozando-rest-api' },
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log', level: 'info' })
+    new winston.transports.File({ filename: 'combined.log', level: 'info' }),
+    new winston.transports.MongoDB({
+      level: 'error',
+      db: `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`,
+      collection: ERROR_LOGS_COLLECTION,
+    }),
+    new winston.transports.MongoDB({
+      level: 'warn',
+      db: `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`,
+      collection: WARNING_LOGS_COLLECTION,
+    }),
   ]
 });
 
-logger.add(new winston.transports.MongoDB(mongoDbOptions));
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
